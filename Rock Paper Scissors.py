5ec7569f8ab42b5da9a0224675298584c7ca2194
SkellyTheDucc <grikee1234@gmail.com>
2024-01-03 12:21:00 -0500

    sdad

---
+++ Rock Paper Scissors.py
@@ -0,0 +1,39 @@
+import random
+
+choice = ["Rock", "Paper", "Scissors"]
+playerchoice = input("Rock, Paper, or Scissors:")
+computerchoice = choice[random.randint(0,2)]
+
+def rockpaperscissors(a,b) :
+    game = 0
+    while game == 0 :
+        if a == b :
+            print("You both chose", b.capitalize() + ". You tied!")
+            print("Try Again!")
+            a = input("Rock, Paper, or Scissors:").upper()
+        elif a == "ROCK" :
+            if b == "PAPER" :
+                print("The Computer chose", b.capitalize() + ". You lose!")
+                game = 1
+            else :
+                print("The Computer chose", b.capitalize() + ". You win!")
+                game = 1
+        elif a == "PAPER" :
+            if b == "SCISSORS" :
+                print("The Computer chose", b.capitalize() + ". You lose!")
+                game = 1
+            else :
+                print("The Computer chose", b.capitalize() + ". You win!")
+                game = 1
+        elif a == "SCISSORS" :
+            if b == "ROCK" :
+                print("The Computer chose", b.capitalize() + ". You lose!")
+                game = 1
+            else :
+                print("The Computer chose", b.capitalize() + ". You win!")
+                game = 1
+        else :
+            print("Invalid Choice")
+            a = input("Rock, Paper, or Scissors:").upper()
+
+rockpaperscissors(playerchoice.upper(), computerchoice.upper())
