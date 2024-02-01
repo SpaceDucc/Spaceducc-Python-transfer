2676b2ae022e785322a99013c3404066c903fa6d
SkellyTheDucc <grikee1234@gmail.com>
2024-02-01 12:01:36 -0500

    sdad

---
+++ Stock Market game.py
@@ -0,0 +1,36 @@
+import random
+
+day = 1
+hour = 1
+money = 0
+stock1price = 150 #bluechip
+stock2price = 30 #crypto
+stock3price = 15 #medium
+stock4price = 20 #medium
+stock5price = 30 #medium
+stock6price = 40 #medium
+stock7price = 2 #penny
+stock8price = 3 #penny
+goal = 2000
+
+game = 0
+
+while game != 1 :
+    print("Welcome to The Miniture Stock Market Game by SPDC Technologies.")
+    difficulty = input("Please select a difficulty(easy, medium, or hard):")
+    if difficulty.lower() == "easy" :
+        money = 300
+        #easy difficulty gamemode
+        game = 1
+    elif difficulty.lower() == "medium" :
+        money = 200
+        #medium difficulty gamemode
+        game = 1
+    elif difficulty.lower() == "hard" :
+        money = 100
+        #hard difficulty gamemode
+        game = 1
+    else :
+        print("invalid difficulty")
+        input("Please select a difficulty(easy, medium, or hard):")
+
