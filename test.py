0960837258370cb39ec9ef3b56d382711d5acde0
SkellyTheDucc <grikee1234@gmail.com>
2024-02-22 11:38:34 -0500

    hhhh

--- test.py
+++ test.py
@@ -1,7 +1,17 @@
 from turtle import *
+from graphics import *
 
-for steps in range(100) :
-    for c in ('blue', 'red', 'green') :
-        color(c)
-        forward(steps)
-        right(30)
+def test1() :
+    win = GraphWin("Mywindow", 500, 500)
+
+    win.getMouse()
+    win.close()
+
+test1()
+def test2() :
+    for steps in range(100) :
+        for c in ('blue', 'red', 'green') :
+            color(c)
+            forward(steps)
+            right(30)
+
